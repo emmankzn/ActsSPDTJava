98b86589146e6c3399e7fa36e57722cb1ee5813f
EmmanPh <156900513+seii03@users.noreply.github.com>
2024-10-08 08:20:32 +0800

    Commit for Array

---
+++ ArrayPractice/src/arrayPracPacks/ArrayPractice.java
@@ -0,0 +1,21 @@
+package arrayPracPacks;
+
+public class ArrayPractice {
+	public static void main(String [] args) {
+		
+		
+		String email[] = {"Emman@gmail.com","ced@gmail.com","chan@gmail.com"};
+		String password[] = {"123","456","789"};
+		String username[] = {"emmanzxc","ced435","chany"};
+		
+		int i = 1;
+
+		System.out.println("Credentials:");
+		System.out.println("index   : " + i);
+		System.out.println("Email   : " + email[i]);
+		System.out.println("Password: " + password[i]);
+		System.out.println("Username: " + username[i]);
+		
+		
+		}
+}
